82163042f622348d1f6ce3f5f731bc01e7793eb6
faggian <felipesantosfaggian@gmail.com>
2023-10-04 22:02:50 -0300

    literals

---
+++ literals.py
@@ -0,0 +1,25 @@
+print(0o123, "octadecimal")
+
+print("-------")
+
+print(0x123, "hexadecimal")
+
+print("-------")
+
+print(3E8, "exponent")
+
+print("-------")
+
+print("I like \"Monty Python\"")
+
+print("-------")
+
+print('I like "Monty Python"')
+
+print("-------")
+
+print('I\'m Monty Python.')
+
+print("-------")
+
+print("\"I'm\"","\n\"\"learning\"\"","\n\"\"\"Python\"\"\"")
