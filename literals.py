aacd52016b4ffc310c2123bb3f7d4613ff35dd41
faggian <felipesantosfaggian@gmail.com>
2023-10-05 20:28:16 -0300

    operators

--- literals.py
+++ literals.py
@@ -1,25 +1,90 @@
 print(0o123, "octadecimal")
 
-print("-------")
 
 print(0x123, "hexadecimal")
 
-print("-------")
+
 
 print(3E8, "exponent")
 
-print("-------")
+
 
 print("I like \"Monty Python\"")
 
-print("-------")
 
 print('I like "Monty Python"')
 
-print("-------")
+
 
 print('I\'m Monty Python.')
 
-print("-------")
+
 
 print("\"I'm\"","\n\"\"learning\"\"","\n\"\"\"Python\"\"\"")
+
+
+print(2+2)
+
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+
+
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+print(6 // 4)
+print(6. // 4)
+
+
+print(-6 // 4)
+print(6. // -4)
+
+print(14 % 4)
+
+print(12 % 4.5)
+
+
+print(-4 +4)
+print(-4. + 8)
+
+
+print(-4 - 4)
+print(4. - 8)
+print(-1.1)
+
+
+print(9 % 6 % 2)
+
+
+print(2 ** 2 ** 3)
+
+print(2 * 3 % 5)
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+
+print(4 ** -1)
+
+print(2 ** 2 ** 3)
+
+
+
+
+
+
+
+
+
