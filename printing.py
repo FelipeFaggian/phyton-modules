40b452ee060fdd4994a4fe36f1c4bd6a0d715844
faggian <felipesantosfaggian@gmail.com>
2023-10-04 21:02:43 -0300

    print-function

---
+++ printing.py
@@ -0,0 +1,48 @@
+print("My" "name", "is", "Monty", "Python.", sep="-")
+
+
+print("-----")
+
+
+
+print("My name is ", end="")
+print("Monty Python.")
+
+
+print("-----")
+
+
+print("My name is", "Python.")
+print("Monty Python.")
+
+
+
+print("-----")
+
+
+print("The itsy bitsy spider" , "climbed up" , "the waterspout.")
+
+print("-----")
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+print("-----")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+
+print("-----")
+
+print("     *", "\n   *   *", "\n  *     *","\n *       *","\n***     ***","\n  *     *","\n  *     *","\n  *******", sep="")
+
+print("     *        " * 2) 
+print("   *   *      " * 2)
+print("  *     *     " * 2)
+print(" *       *    " * 2)
+print("***     ***   " * 2)
+print("  *     *     " * 2)
+print("  *     *     " * 2)
+print("  *******     " * 2)
