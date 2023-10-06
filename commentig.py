e2adb18c08217c04b2516c3d36000c4375c0182a
faggian <felipesantosfaggian@gmail.com>
2023-10-06 19:25:21 -0300

    commentig

---
+++ commentig.py
@@ -0,0 +1,11 @@
+# print("testing")
+
+
+#this program computes the number of seconds in a given number of hours
+# this program has been written two days ago
+number_hours = 3 
+number_seconds_1_hour = 3600 
+print("Hours: ", number_hours) 
+print("Seconds in Hours: ", number_hours * number_seconds_1_hour)
+print("Goodbye!")
+
--- variables.py
+++ variables.py
@@ -66,5 +66,10 @@ x = float(x)
 y = 3 * x ** 3 - 2 *  x ** 2 + 3 * x - 1
 print("y =", y)
 
+var = "007"
+print("Agent " + var)
+
+
+
 
 
