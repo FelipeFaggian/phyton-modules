e222b5d76a934f7142145f66600551ea8bbbec53
faggian <felipesantosfaggian@gmail.com>
2023-10-05 21:58:41 -0300

    variables

---
+++ variables.py
@@ -0,0 +1,70 @@
+var = 1
+print(var)
+
+var = "3.8.5"
+print("Python version: " + var)
+
+var = 1
+print(var)
+var = var + 1
+print(var)
+
+var = 100
+var = 200 + 300
+print(var)
+
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+john = 12 // 4
+mary = 2 + 3
+adam = 2 * 3
+print("John's apples = ", john, "Mary's apples = ", mary,"Adam's apples = ", adam)
+total_apples = john + mary + adam
+print("Total number of apples: ", total_apples)
+
+
+x = 1
+sheep = 0
+x *= 2
+sheep += 1
+print(x, sheep)
+
+
+
+kilometers = 12.25
+miles = 7.38
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+
+
+usd = 2000
+eur = 400
+usd_to_eur = usd * 0.95
+eur_to_usd = eur / 0.95
+print(usd, "USD equals to", round(usd_to_eur, 2), "EUR")
+print(eur, "EUR equals to", round(eur_to_usd, 2), "USD")
+
+
+f = 33.8
+c = 33.8
+fahrenheit_to_celsius = (f - 32) * 5/9
+celsius_to_fahrenheit = (c * 9/5) + 32
+print(f,"F equals to ", round(fahrenheit_to_celsius,2),"C", sep="")
+print(c, "C equals to ", round(celsius_to_fahrenheit,2), "F", sep="")
+
+
+x =  -1
+x = float(x)
+# write your code below
+y = 3 * x ** 3 - 2 *  x ** 2 + 3 * x - 1
+print("y =", y)
+
+
+
