114731a9aee0bf8bb0d154cf1f7814e0f375b99c
faggian <felipesantosfaggian@gmail.com>
2023-10-07 20:35:16 -0300

    making-decisions

--- input.py
+++ input.py
@@ -88,3 +88,24 @@ else:
 #key takeaways input review
 name = input("Enter your name: ")
 print("Hello, " + name + ". Nice to meet you!")
+
+#press to end
+name = input("Enter your name: ")
+print("Hello, " + name + ". Nice to meet you!")
+print("\nPress Enter to end the program.")
+input()
+print("THE END.")
+
+# concatenation
+num_1 = input("Enter the first number: ") # Enter 12
+num_2 = input("Enter the second number: ") # Enter 21
+print(num_1 + num_2) # the program returns 1221
+
+# replication
+my_input = input("Enter something: ") # Example input: hello
+print(my_input * 3) # Expected output: hellohellohello
+
+# test input tyoe
+x = input("Enter a number: ") # The user enters 2
+print(type(x)
+# output: <class 'str'> (In first moment, an input will always return a string)
---
+++ making-decisions.py
@@ -0,0 +1,15 @@
+#a==b (Is a equal to b?)
+
+#2 == 2. (Python can convert and realize that's is a True equal)
+
+#equality
+var = 0  # Assigning 0 to var
+print(var == 0)
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+#inequality
+var = 0  # Assigning 0 to var
+print(var != 0)
+var = 1  # Assigning 1 to var
+print(var != 0)
